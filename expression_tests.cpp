cfa0410a4ab1be9b36cfc10a01b1851112ac0c64
Joe Hamilton <joe1640@vt.edu>
2018-08-26 20:40:19 -0400

    - Reformatted tests with proper indentation - Added test case for new "e" symbol

--- expression_tests.cpp
+++ expression_tests.cpp
@@ -3,27 +3,27 @@
 #include "expression.hpp"
 
 TEST_CASE( "Test default expression", "[expression]" ) {
-
-  Expression exp;
-
-  REQUIRE(!exp.isHeadNumber());
-  REQUIRE(!exp.isHeadSymbol());
+    
+    Expression exp;
+    
+    REQUIRE(!exp.isHeadNumber());
+    REQUIRE(!exp.isHeadSymbol());
 }
 
 TEST_CASE( "Test double expression", "[expression]" ) {
-
-  Expression exp(6.023);
-
-  REQUIRE(exp.isHeadNumber());
-  REQUIRE(!exp.isHeadSymbol());
+    
+    Expression exp(6.023);
+    
+    REQUIRE(exp.isHeadNumber());
+    REQUIRE(!exp.isHeadSymbol());
 }
 
 
 TEST_CASE( "Test symbol expression", "[expression]" ) {
-
-  Expression exp(Atom("asymbol"));
-
-  REQUIRE(!exp.isHeadNumber());
-  REQUIRE(exp.isHeadSymbol());
+    
+    Expression exp(Atom("asymbol"));
+    
+    REQUIRE(!exp.isHeadNumber());
+    REQUIRE(exp.isHeadSymbol());
 }
 
