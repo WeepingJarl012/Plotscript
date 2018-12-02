a4e37e67d595bf9e2d5bf7e99927c01b37906f94
Joe Hamilton <joe1640@vt.edu>
2018-12-01 19:48:42 -0500

    - Fixed cos and sin for complex numbers

--- environment.cpp
+++ environment.cpp
@@ -299,8 +299,10 @@ Expression sin(const std::vector<Expression> & args){
     double result = 0;
     
     if(nargs_equal(args,1)){
-        if (args[0].isHeadComplex() || args[0].isHeadNumber()){
+        if (args[0].isHeadNumber()){
             result = sin(args[0].head().asNumber());
+        } else if (args[0].isHeadComplex()){
+            return Expression(sin(args[0].head().asComplex()));
         } else {
             throw SemanticError("Error in call to sin: invalid argument");
         }
@@ -317,8 +319,10 @@ Expression cos(const std::vector<Expression> & args){
     double result = 0;
     
     if(nargs_equal(args,1)){
-        if (args[0].isHeadComplex() || args[0].isHeadNumber()){
+        if (args[0].isHeadNumber()){
             result = cos(args[0].head().asNumber());
+        } else if (args[0].isHeadComplex()){
+            return Expression(cos(args[0].head().asComplex()));
         } else {
             throw SemanticError("Error in call to cos: invalid argument");
         }
